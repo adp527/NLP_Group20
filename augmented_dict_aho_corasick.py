f41e3f518237ed8f9e728031e5102fc979b28987
adp527 <60910405+adp527@users.noreply.github.com>
2022-05-05 07:42:42 -0400

    Add files via upload

--- augmented_dict_aho_corasick.py
+++ augmented_dict_aho_corasick.py
@@ -1,10 +1,11 @@
-from turtle import st
+from xml.etree.ElementInclude import include
 import jsonlines
 import string
 import ahocorasick as ac
 
-dictionary_path = "idioms_wiktionary_augmented.txt"
+dictionary_path = "idioms_extended_augmented.txt"
 corupus_path = "magpie_unfiltered.jsonl"
+#corupus_path = "magpie_test_data.jsonl"
 
 A = ac.Automaton()
 
@@ -18,6 +19,23 @@ with open(dictionary_path, "r") as f:
         idx += 1
 
 A.make_automaton()
+
+
+def process(context):
+    context = context.lower()
+    res = ""
+    indices = []
+    ptr = 0
+    for x in context:
+        if x in string.ascii_letters or x in string.digits or x == "\'":
+            res += x
+            indices.append(ptr)
+        elif x in string.whitespace and (len(res) == 0 or res[-1] not in string.whitespace):
+            res += " "
+            indices.append(ptr)
+        ptr += 1
+
+    return res, indices
 
 # Load corpus
 instances = 0
@@ -39,8 +57,15 @@ with jsonlines.open(corupus_path) as reader:
             used_start_indices = {}
 
             # Aho-Corasick is rediculously fast
+
+            found_match = False
+
             for context in context_list:
-                context = context.lower()
+                orig_context = context
+                curr_idiom_end = idiom_end
+                while curr_idiom_end < len(context) and curr_idiom_end >= 0 and context[curr_idiom_end] not in string.ascii_letters:
+                    curr_idiom_end -= 1
+                context, indices = process(context)
                 for end_index, (insert_order, original_value) in A.iter(context):
                     start_index = end_index - len(original_value) + 1
                     if start_index not in used_start_indices:
@@ -50,10 +75,11 @@ with jsonlines.open(corupus_path) as reader:
                         used_start_indices[start_index] = end_index
                         #In the final version, where we need to return the actual indices, we can defer to the longest match
 
-                    if idiom_begin == start_index and idiom_end == end_index:   
+                    if idiom_begin == indices[start_index] and curr_idiom_end == indices[end_index]:
                         accurately_identified += 1
-                    elif idiom_begin == start_index and idiom_end - 1 == end_index and context[idiom_end] == '\u2019':
-                        accurately_identified += 1
+                        found_match = True
+        
+      
 
 print("")
 print(str(accurately_identified) + " " + str(total_identified))
--- dictionary_extension.py
+++ dictionary_extension.py
@@ -3,13 +3,26 @@
 from matplotlib.pyplot import stem
 
 
-original_dictionary_path = "idioms_wiktionary.txt"
-modified_dictionary_path = "idioms_wiktionary_augmented.txt"
+original_dictionary_path = "idioms_extended.txt"
+modified_dictionary_path = "idioms_extended_augmented.txt"
 
 VERBS_ONLY = False
-IGNORE_CUSTOM_DERIVATIONS = True
+IGNORE_CUSTOM_DERIVATIONS = False
 
+"""
+Statistics from build as of 04/26/2022, 2:25pm
+VERBS ONLY, NO CUSTOM
+Precision: 0.19508523356209873
+Recall: 0.5306515717210647
 
+NOT VERBS ONLY, NO CUSTOM
+Precision: 0.20031432420296363
+Recall: 0.5372756834878959
+
+VERBS ONLY, WITH CUSTOM
+Precision: 0.14463613550815557
+Recall: 0.5553414428519812
+"""
 
 # Use a Python Dictionary to store word groups and their modified forms
 morphology_dictionary_path = "catvar_english_morphology.txt"
@@ -43,6 +56,8 @@ if not IGNORE_CUSTOM_DERIVATIONS:
         stem_derivations[line[1]].append(line[0])
         root[line[0]] = line[1]
 
+INFLECTION_LIMIT = 8192
+
 def recursive_search(words):
     if len(words) == 0:
         return [""]
@@ -56,6 +71,8 @@ def recursive_search(words):
         for word in prev_result:
             for derivation in stem_derivations[curr_root]:
                 result.append(derivation + " " + word)
+                if len(result) > INFLECTION_LIMIT:
+                    return result
         return result
             
 
@@ -67,25 +84,28 @@ output_file = open(modified_dictionary_path, "w")
 # Load the original dictionary
 print("Beginning dictionary modification...")
 current_line = 0
+total_work = 0
 for line in open(original_dictionary_path, "r"):
     line = line.strip().split()
     print("\rCurrent line: {}".format(current_line), end="")
     current_line += 1
     all_derivations = recursive_search(line)
     for derivation in all_derivations:
-        derivation.replace("n't", " n't")
-        derivation.replace("'ve", " 've")
-        derivation.replace("'ll", " 'll")
-        derivation.replace("'d", " 'd")
-        derivation.replace("'s", " 's")
-        derivation.replace("'re", " 're")
-        derivation.replace("'m", " 'm")
-        derivation.replace("'em", " 'em")
-        derivation.replace("gonna", "gon na")
-        derivation.replace("gotta", "got ta")
+        derivation = derivation.replace("n't", " n't")
+        derivation = derivation.replace("'ve", " 've")
+        derivation = derivation.replace("'ll", " 'll")
+        derivation = derivation.replace("'d", " 'd")
+        derivation = derivation.replace("'s", " 's")
+        derivation = derivation.replace("'re", " 're")
+        derivation = derivation.replace("'m", " 'm")
+        derivation = derivation.replace("'em", " 'em")
+        derivation = derivation.replace("gonna", "gon na")
+        derivation = derivation.replace("gotta", "got ta")
+        total_work += len(derivation)
         output_file.write(derivation + "\n")
 
 output_file.close()
+print("\n" + str(total_work))
 
     
     
